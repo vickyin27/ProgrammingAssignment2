5920ef624111fc2afabb481b28dbfbcb92b46cf4
vickyin27 <vicky.nipun@gmail.com>
2016-09-03 11:57:34 -0700

    Update Week3.R

--- Week3.R
+++ Week3.R
@@ -1,6 +1,7 @@
 ## Week 3 Assignment
 
 ## Function 1
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) { 
   inv <- NULL                             
   set <- function(y) {                    
@@ -15,6 +16,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 ## Function 2
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
   inv <- x$getinverse()
   if(!is.null(inv)) {
